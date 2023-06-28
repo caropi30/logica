1a6814743b4b18f2b6000a650a20d784bce347fc
unknown <caropi30@gmail.com>
2023-06-27 21:48:36 -0400

    ejercciio-2

---
+++ mentoría/ejercicio-2/script.js
@@ -0,0 +1,16 @@
+//Escriba una función que reciba dos parámetros del tipo string.
+//La función deberá retornar la cantidad de apariciones que tiene el segundo parámetro en el primer parámetro.
+
+function two (word, letter){
+    //console.log(word, letter)
+    let quantity = 0
+    for(let i = 0; i<= word.length; i++){
+      //console.log(word[i])
+      if(word[i] === letter)
+        //console.log('coinciden')
+        quantity++
+    }
+    return quantity
+  }
+  
+  two('aleluya', 'a')
