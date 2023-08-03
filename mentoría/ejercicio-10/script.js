2103a14a2f9b479036804e7c2c5e9599a12a4ee3
unknown <caropi30@gmail.com>
2023-08-02 21:26:48 -0400

    feat: parte 1 resolución ejercicio 10 y fix de scope variable nwArr en ejecrcicio 8

--- mentoría/ejercicio-10/script.js
+++ mentoría/ejercicio-10/script.js
@@ -4,11 +4,40 @@
     Escribir una función que reciba un objeto como primer parámetro obj y un array filtros de strings como segundo parámetro. 
     Dado un objeto particular, la función filtrarKeys deberá retornar una lista de las propiedades 
     ( es decir, una lista de keys) que contenga alguno de los elementos del array filtros en su nombre. 
-    Es decir, debería comportarse de la siguiente manera:
 */
+
+
+const objKeys = { "a":1, "b":2, "c":3 }
+const arrFilter = ["b", "i", "a", "o"];
+
+
+const keyFilter = (param1,param2) => {
+    const keyAndArrMatch = []
+    console.log(param1,param2)
+    const keys = Object.keys(param1);
+    console.log(keys);
+    for(let i = 0; i < keys.length; i++){
+        //console.log(keys[i])
+        let singleKey = keys[i];
+        for (let j = 0; j < param2.length; j++){
+            let matchItem = ''
+           // console.log(param2[j])
+            if(param2[j] === singleKey){
+                console.log('coinciden')
+                matchItem = param2[j]
+                console.log(matchItem)
+                keyAndArrMatch.push(matchItem)
+            }
+        }
+       console.log(keyAndArrMatch)
+    }
+}
+
+keyFilter(objKeys, arrFilter)
 
 
 /*PARTE 2:
     Agregar a la función filtrarKeys un tercer parámetro opcional que indique si se desea filtrar por inclusion o exclusion. 
     Por defecto, es decir si ningún parámetro es especificado, la función deberá filtrar por inclusion.
 */
+
