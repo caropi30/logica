201e283072abe759a5f6fc1be6b27d427add1a9b
unknown <caropi30@gmail.com>
2023-09-03 20:53:28 -0300

    fix: corrección de scope para variable arrKeys

--- mentoría/ejercicio-10/script.js
+++ mentoría/ejercicio-10/script.js
@@ -6,13 +6,20 @@
     ( es decir, una lista de keys) que contenga alguno de los elementos del array filtros en su nombre. 
 */
 
+/* PARTE 2:
+    Agregar a la función filtrarKeys un tercer parámetro opcional que indique si se desea filtrar por inclusion o exclusion. 
+    Por defecto, es decir si ningún parámetro es especificado, la función deberá filtrar por inclusion.
+*/
+
 const obj = { a: 1, b: 2, c: 3 }
+const obj2 = { d: 1, x: 2, g: 3 }
 const arr = ['a','b']
-const arrKeys = []
+const arr2 = ['a', 'b', 'd', 'x', 'g']
 
 function filtrarKeys (obj, arrFiltro, inclusion = true){
   let key = ''
   const keys = Object.keys(obj)
+  const arrKeys = []
 
   if (!inclusion) {
      for (let i = 0; i < keys.length; i++) {
@@ -21,6 +28,7 @@ function filtrarKeys (obj, arrFiltro, inclusion = true){
              arrKeys.push(key)
          }
      }
+    return arrKeys
  } else {
   for (let i = 0; i < keys.length; i++) {
       key = keys[i]
@@ -28,15 +36,22 @@ function filtrarKeys (obj, arrFiltro, inclusion = true){
           arrKeys.push(key)
       }
     }
+    return arrKeys
   }
-  return arrKeys
 }
 
 console.log(filtrarKeys(obj, arr))
+console.log(filtrarKeys(obj, arr, true))
 console.log(filtrarKeys(obj, arr, false))
+console.log(filtrarKeys(obj2, arr2))
+console.log(filtrarKeys(obj2, arr2, true))
+console.log(filtrarKeys(obj2, arr2, false))
+console.log(filtrarKeys(obj2, arr))
+console.log(filtrarKeys(obj2, arr, true))
+console.log(filtrarKeys(obj2, arr, false))
+console.log(filtrarKeys(obj, arr2))
+console.log(filtrarKeys(obj, arr2, true))
+console.log(filtrarKeys(obj, arr2, false))
 
-/* PARTE 2:
-    Agregar a la función filtrarKeys un tercer parámetro opcional que indique si se desea filtrar por inclusion o exclusion. 
-    Por defecto, es decir si ningún parámetro es especificado, la función deberá filtrar por inclusion.
-*/
+
 
