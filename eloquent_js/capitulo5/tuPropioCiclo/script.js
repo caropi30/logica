59a23446ecf343ec6b35abf50b2c54f52f20c0ea
unknown <caropi30@gmail.com>
2024-01-18 20:21:56 -0300

    feat: nueva resolucion tuPropioCiclo

---
+++ eloquent_js/capitulo5/tuPropioCiclo/script.js
@@ -0,0 +1,41 @@
+/*
+    Escriba una función de orden superior llamada ciclo que proporcione algo así como una declaración de ciclo for. 
+    Esta toma un valor, una función de prueba, una función de actualización y un cuerpo de función. En cada iteración, primero ejecuta 
+    la función de prueba en el valor actual del ciclo y se detiene si esta retorna falso. Luego llama al cuerpo de función, dándole el valor actual. 
+    Y finalmente, llama a la función de actualización para crear un nuevo valor y comienza desde el principio.
+
+    Cuando definas la función, puedes usar un ciclo regular para hacer los ciclos reales.
+
+    loop(3, n => n > 0, n => n - 1, console.log);
+    // → 3
+    // → 2
+    // → 1
+*/
+
+// function esPar(numEnteroPositivo) {
+//     function siEsPar(numActual) {
+//         if (numActual === 0) {
+//             return verdadero
+//         } else if (numActual === 1) {
+//             return falso
+//         } else {
+//             return siEsPar(numActual - 2)
+//         }
+//     }
+//     return siEsPar(numEnteroPositivo)
+// }
+
+for(let  = 0; i > 0; i--)
+
+function ciclo (valor, funcionPrueba, funcionActualizacion) {
+  function funcionPrueba(valorActual){
+    return funcionActualizacion(valorActual)
+  }
+  return funcionPrueba(valor)
+}
+
+ciclo(
+    3,
+    (n) => n > 0,
+    (n) => n - 1,
+)
